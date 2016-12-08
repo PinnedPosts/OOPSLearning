37ead72d6855b781f52b4dfb4d972c69b58f51c4
Dinesh Krishnan <pinnedposts@gmail.com>
2016-12-08 16:16:31 +0530

    Added inheritance example

---
+++ OOPSWithCSharp/OOPSWithCSharp/Class/Student.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPSWithCSharp.Class
+{
+    /// <summary>
+    /// This is the class representation
+    /// </summary>
+    public class Student
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Division { get; set; }
+        public string Role { get; set; }
+    }
+}
--- OOPSWithCSharp/OOPSWithCSharp/Program.cs
+++ OOPSWithCSharp/OOPSWithCSharp/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using OOPSWithCSharp.Class;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,8 +9,17 @@ namespace OOPSWithCSharp
 {
     class Program
     {
+        /// <summary>
+        /// Entry point of the app.The process of starting up is called bootstrapping
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
+            //This is an object of student class
+            Student zee = new Student();
+            zee.Name = "Zeeshan";
+            zee.Age = 40;
+            zee.Division = "4th Std";
         }
     }
 }
